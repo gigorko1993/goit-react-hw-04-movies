5fc022962d382842b7e6fb0df348da0ed6929f40
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2021-09-04 09:13:41 +0300

    add home page, page 404, fetch movies api

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import * as fetchMovies from '../../api';
+
+export default function HomeView() {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    fetchMovies.fetchTrendMovies().then(res => setMovies(res.results));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Trending today</h1>
+      <ul>
+        {movies &&
+          movies.map(movie => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>
+                {movie.title}
+                {movie.name}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
